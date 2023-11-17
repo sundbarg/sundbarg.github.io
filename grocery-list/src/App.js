2bebf39380e62f20f411b9ed4ad223fd1fe40453
Daniel Sundberg <daniel.sundberg@resurs.se>
2023-11-17 16:48:08 +0100

    Fix?

--- grocery-list/src/App.js
+++ grocery-list/src/App.js
@@ -1,19 +1,47 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  const add = (e) => {
+    e.preventDefault();
+    if (!item) {
+      return;
+    }
+    if (items.indexOf(item) !== -1) return;
+    setItems((items) => {
+      var tmpItems = [...items, item];
+
+      return tmpItems.sort((a, b) => sortByName(a, b));
+    });
+  };
+
+  const remove = (index) => {
+    setItems((items) => items.filter((_, i) => i !== index));
+  };
+
+  function sortByName(a, b) {
+    return a > b;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-         Grocery List
-
-         <body>        
-          <button onclick="addItem()">Add</button>
-          <ul id="groceryList"></ul>
-          <script src="src/js/script.js"></script>
-        </body>
-      </header>
-      
+      <form onSubmit={add}>
+        <br/>
+          <input value={item} onChange={(e) => setItem(e.target.value)} /> <button type="submit">add item</button>
+      </form>
+      {items.map((item, index) => {
+        return (
+          <div key={item}>
+            <p>
+              {item} <button onClick={() => remove(index)}>x</button>
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
